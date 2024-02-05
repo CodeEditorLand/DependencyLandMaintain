8375b25f095b45998e3b925856d0b588ecd40c60
Nikola Hristov <nikola@nikolahristov.tech>
2024-02-05 04:41:58 +0200

    squash!

--- Source/Fn/Binary/Maintain.rs
+++ Source/Fn/Binary/Maintain.rs
@@ -4,7 +4,7 @@ use clap::{arg, Command};
 use Library::Fn::Cache;
 
 fn main() {
-	let matches = Command::new("🌄 Maintain Land.")
+	let matches = Command::new("🤸🏽 Maintain Land.")
 		.version(env!("CARGO_PKG_VERSION"))
 		.author("Nikola R. Hristov <nikola@nikolahristov.tech>")
 		.about("Build")
