59db5e572b4117efaa2ecd1158949868c6c6cb95
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-04 10:44:49 +0300

    squash!

--- Source/Fn/Action/Append/Detail.rs
+++ Source/Fn/Action/Append/Detail.rs
@@ -42,7 +42,7 @@ fn main() {
             "url": "https://nikolahristov.tech"
         },
         "scripts": {
-            "prepublishOnly": "Build 'Source/**/*.ts'"
+            "Build": "Build 'Source/**/*.ts'"
         },
         "devDependencies": {
             "@playform/build": "0.3.7"
